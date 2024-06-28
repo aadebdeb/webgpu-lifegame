0bad872eb1caa8e1903cb2721410bb05eeed7ea7
Atsushi Asakura <aa.debdeb@gmail.com>
2024-06-28 22:50:33 +0900

    add base for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite';
 import plainText from 'vite-plugin-plain-text';
 
 export default defineConfig({
+  base: process.env.GITHUB_PAGES ? '/webgpu-lifegame/' : '/',
   plugins: [
     plainText([/\.wgsl$/]),
   ],
